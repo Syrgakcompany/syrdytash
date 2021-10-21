bf20050bd0e2e982234d7d7c4b4e0e068b3a1adc
Syrgakcompany <92498012+Syrgakcompany@users.noreply.github.com>
2021-10-21 10:26:12 +0600

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
 
-    <button type="submit">Login</button>
+    <a href="#" class="next">Login &raquo;</a>
     <label>
       <input type="checkbox" checked="checked" name="remember"> Remember me
     </label>
