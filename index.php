ea45dd4c81bf479ba58b506c0b2f79e207121ca1
Syrgakcompany <92498012+Syrgakcompany@users.noreply.github.com>
2021-10-28 10:59:16 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,48 @@
- <form action="action_page.php" method="post">
-  <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Nurtenir</title>
+</head>
+<body>
+    <h1>My summary</h1>
+</body>
+</html>
 
-  <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
+</head>
+<body>
+    <h1>babanov sygrak</h1>
+</body>
+</html>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
 
-    <a href="syrgak.html" class="next">login sdelay brat &raquo;</a>
-   
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
+</head>
+<body>
+	<h1>Handsome boy </h1>
+	 <img src="Syrgak.jpeg"> 
+	<a href="index.html">home</a>
+<body>
+</html>
+</head>
+<body>
+    <h1>23.06.2004</h1>
+</body>
+</html>
 
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
-</form> 
+</head>
+<body>
+    <h1>i am senior collage student at it bussines college in International Ala-Too university </h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>My department is Computer system and comploxes</h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>Next year i am planning to apply MIT</h1>
+</body>
+</html>
