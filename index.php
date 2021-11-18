17591c6654f88d9470fd47f848a663d2ff37bbd3
Syrgakcompany <92498012+Syrgakcompany@users.noreply.github.com>
2021-11-18 13:13:32 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Syrgak</title>
+</head>
+<body>
+    <h1>My summary</h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>Babanov Syrgak</h1>
+
+
+n
+
+
+</body>
+</html>
+
+
+</head>
+<body>
+	                           <img src="syrgak.jpg"> 
+	                        
+<body>
+</html>
+
+</head>
+<body>
+	  <a href="https://web.whatsapp.com/">WhatsApp</a>
+	 <a href="https://www.instagram.com/s1414ll/">Instagram</a>
+	 <a href="https://web.telegram.org/z/">Telegram</a>
+	
+<body>
+</html>	
+	
+</head>
+<body>
+    <h1>23.06.2004</h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>My phone number;0502454545</h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>My adress: maldybaeva27</h1>
+</body>
+</html>
+
+
+</head>
+<body>
+    <h1>i am senior collage student at it bussines college in International Ala-Too university </h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>My department is Computer system and comploxes</h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>Next year i am planning to apply MIT</h1>
+</body>
+</html>
